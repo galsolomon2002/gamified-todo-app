cae88709f8caf49ab6c98251bf4f68d923a4dad8
Shani sahar <shanisahar@Shanis-MacBook-Air-2.local>
2025-08-08 00:13:14 +0300

    Save local changes before pull

---
+++ constants/gamification.ts
@@ -0,0 +1,16 @@
+// constants/gamification.ts
+export const SUGGESTED_SMALL_REWARDS = [
+  "שוקו חם",
+  "פרק של סדרה",
+  "10 דק׳ טיקטוק/אינסטה",
+  "קפה ומאפה",
+  "שיחת חברה קצרה",
+];
+
+export const SUGGESTED_BIG_REWARDS = [
+  "מסעדה טובה",
+  "ספא",
+  "טיול יום",
+  "בגד חדש",
+  "לילה במלון",
+];
